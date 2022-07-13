7261035eb0f93ff6230a7059291c60c3f5160a4a
LorisP96 <lorispalazzo96@gmail.com>
2022-07-13 12:41:01 +0200

    web app slider

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,8 +1,10 @@
 <template>
   <main>
-    <div class="card-container">
 
-      <h2 class="categories">Film</h2>
+    <h2 class="categories">Film</h2>
+    <div class="card-container">
+      
+      
       <div class="card" v-for="card, index in arrayFilmResult" :key="'a' + index">
         <div class="info-card">
           <h3>{{card.title}}</h3>
@@ -12,22 +14,26 @@
             <span v-for="star, index in (5 - fixStar(card.vote_average))" :key="'d' + index"><i class="fa-regular fa-star"></i></span>
             <span>numero di recensioni: {{card.vote_count}}</span>
           </div>
-          <div class="original-text">{{card.original_title}}</div>
+          <div class="original-text">{{card.overview}}</div>
         </div>
         <img :src="'https://image.tmdb.org/t/p/w342' + card.poster_path" :alt="card.title">
       </div>
+    </div>
 
-      <h2 class="categories">Serie TV</h2>
+    <h2 class="categories">Serie TV</h2>
+    <div class="card-container">
+
+      
       <div class="card" v-for="cardtv, index in arrayTvResult" :key="'b' + index"> 
         <div class="info-card">
           <h3>{{cardtv.name}}</h3>
           <img :src="'https://countryflagsapi.com/svg/' + fixFlag(cardtv.original_language)" alt="">
           <div class="vote">
-            <span class="gold" v-for="star, index in fixStar(cardtv.vote_average)" :key="'c' + index"><i class="fa-solid fa-star"></i></span>
-            <span v-for="star, index in (5 - fixStar(cardtv.vote_average))" :key="'d' + index"><i class="fa-regular fa-star"></i></span>
+            <span class="gold" v-for="index in fixStar(cardtv.vote_average)" :key="'c' + index"><i class="fa-solid fa-star"></i></span>
+            <span v-for="index in (5 - fixStar(cardtv.vote_average))" :key="'d' + index"><i class="fa-regular fa-star"></i></span>
             <div>numero di recensioni: {{cardtv.vote_count}}</div>
           </div>
-          <div class="original-text">{{cardtv.original_name}}</div>
+          <div class="original-text">{{cardtv.overview}}</div>
         </div>
         <img :src="'https://image.tmdb.org/t/p/w342' + cardtv.poster_path" :alt="cardtv.name">
       </div>
@@ -73,76 +79,79 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.card-container {
-  display: flex;
-  flex-wrap: wrap;
-  width: calc(100% - 80px);
-  margin: 0 auto;
-
+main {
+  height: calc(100vh - 70px);
   .categories {
-      width: 100%;
-      margin: 40px 0;
-    font-size: 30px;
-    }
+    width: 100%;
+    padding: 20px 40px;
+    font-size: 28px;
+  }
+  .card-container {
+    display: flex;
+    width: calc(100% - 80px);
+    margin: 0 auto;
+    height: calc(50% - 72px);
+    overflow-x: auto;
 
-  .card {
-    width: calc((100% / 6) - 10px);
-    margin: 5px;
-    aspect-ratio: 2/3;
-    position: relative;
-
-    img {
-      display: block;
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-      object-position: center;
-    }
-
-    .info-card {
-      color: white;
-      position: absolute;
-      top: 0;
-      right: 0;
-      left: 0;
-      bottom: 0;
-      opacity: 0;
-      padding: 20px;
-      overflow-y: auto;
-      .original-text {
-        font-size: 14px;
-        margin: 10px 0;
-      }
+    .card {
+      width: calc((100% / 6) - 10px);
+      margin: 5px;
+      aspect-ratio: 2/3;
+      position: relative;
 
       img {
-        width: 30px;
-        aspect-ratio: 1/1;
-        border-radius: 50%;
-        height: auto;
-        margin: 10px 0;
+        display: block;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        object-position: center;
+      }
+
+      .info-card {
+        color: white;
         position: absolute;
-        bottom: 20px;
-        right: 20px;
-        border: 3px solid white;
-      }
-
-      .vote {
-        font-size: 10px;
-        line-height: 20px;
-        padding-top: 8px;
-
-        .gold {
-          color: goldenrod;
+        top: 0;
+        right: 0;
+        left: 0;
+        bottom: 0;
+        opacity: 0;
+        padding: 20px;
+        overflow-y: auto;
+        .original-text {
+          font-size: 14px;
+          margin: 10px 0;
         }
 
-        span {
-          font-size: 16px;
+        img {
+          width: 25px;
+          aspect-ratio: 1/1;
+          border-radius: 50%;
+          height: auto;
+          margin: 10px 0;
+          position: absolute;
+          top: 0;
+          right: 5px;
+          border: 2px solid white;
+        }
+
+        .vote {
+          font-size: 10px;
+          line-height: 20px;
+          padding-top: 8px;
+
+          .gold {
+            color: goldenrod;
+          }
+
+          span {
+            font-size: 16px;
+          }
         }
       }
-    }
-    &:hover .info-card {
-      opacity: 1;
-      background-color: rgba($color: black, $alpha: 0.5);
+      &:hover .info-card {
+        opacity: 1;
+        background-color: rgba($color: black, $alpha: 0.5);
+      }
     }
   }
 }
