ce5b70a20ea742e2a7f0b0f446ad5b76238899bc
LorisP96 <lorispalazzo96@gmail.com>
2022-09-25 03:20:05 +0200

    new deploy

---
+++ src/deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+set -e
+
+npm run build
+
+cd dist
+
+git init
+
+git add .
+
+git commit -m "New Deploy"
+
+git push -f https://github.com/macs-massimopiazza/simie-66.git master:gh-pages
+
+cd ..
